183dcfd89fe61212e1b37cea46ab62a287f18e5d
jvarghe <vcs.inbox@outlook.com>
2023-06-20 01:06:57 +0000

    Add the 'wordSearch()' function to 'wordsearch.js'.

--- wordsearch.js
+++ wordsearch.js
@@ -39,5 +39,68 @@
 
 
 
+// THE `wordSearch()` FUNCTION
+//
+// This function takes in two variables: `letters` and `word`. `letters` is a
+// multi-dimensional array. `word` is just a string, for which you will be
+// searching `letters`. The word can appear either horizontally or vertically,
+// but not diagonally or in reverse order. If the word is found, `wordSearch`
+// will return `true`, or else it will return `false`.
+const wordSearch = (letters, word) => {
+
+  // CONCATENATING CHARACTERS TO CREATE "WORDS"
+
+  // HORIZONTAL CONCATENATION
+  // Iterates over the multi-dimensional array, line-by-line (sub-array by
+  // sub-array) and joins together all the characters on that line into a single
+  // word. It returns an multi-dimensional array of words, each word being the
+  // result of characters joined together horizontally.
+  const horizontallyJoinedWords = letters.map(letter => letter.join(''));
+  // console.log(horizontallyJoinedWords);
+
+
+  // VERTICAL CONCATENATION
+  // Tranpose the matrix so that its rows become columns:
+  const transposedMatrix = transpose(letters);
+  // console.log(transposedMatrix);
+
+  // Do a join again, this time on the transposed matrix, creating words from
+  // what were columns in the original matrix.
+  const verticallyJoinedWords = transposedMatrix.map(letter => letter.join(''));
+  console.log(verticallyJoinedWords);
+
+
+  // SEARCH THE "JOINED ARRAYS" FOR WORDS
+  // `horizontallyJoinedWords` and `verticallyJoinedWords` should be
+  // multi-dimensional arrays, each one containing sub-arrays which consists of
+  // strings—which are the now-joined together characters from before. Let's
+  // search them to see if the `word` exists in one of the sub-arrays.
+
+
+  // SEARCH THE HORIZONTAL "WORDS"
+  for (let element of horizontallyJoinedWords) {
+
+    if (element.includes(word)) {
+      return true;
+    }
+
+  }
+
+  // SEARCH THE VERTICAL "WORDS"
+  for (let element of verticallyJoinedWords) {
+    if (element.includes(word)) {
+      //console.log(element);
+      return true;
+    }
+  }
+
+
+  // If the search of both the horizontal and vertical lists, did not return
+  // any positive values, the word was NOT found. Return `false`.
+  return false;
+};
+
+
+
 // EXPORTS
 module.exports = wordSearch;
